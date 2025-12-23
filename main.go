c7f5b7eef5780d6403defd80d993690895847ffe
jirbthagoras <hansjabriel@gmail.com>
2025-12-23 10:12:11 +0700

    learn parsing

--- main.go
+++ main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+// limiting the function's access by mengerucutkan type ke io.ReadCloser
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer close(out)
+		defer f.Close()
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err == io.EOF {
+				break
+			}
+
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[1+i:]
+				out <- str
+				str = ""
+			}
+
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			out <- str
+		}
+
+	}()
+
+	return out
+}
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	f, err := os.Open("./message.txt")
+	if err != nil {
+		log.Fatal("error", "error", err)
+	}
+
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
+	}
+
 }
